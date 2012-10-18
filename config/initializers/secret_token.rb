251c331619cae2f1ba0192e7732f78dcfd1a6f67
ruemic <rumicdesign@gmail.com>
2012-10-17 19:15:26 -0700

    playing with icon fonts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foundation::Application.config.secret_token = '12c7ca61f9d368e2c470da519dc175ffe663e223c174978d87dfa7e489c4b542ca419333042bc7529a11033c10639597411d9365c44c76c34248ef7b2939d2e5'
